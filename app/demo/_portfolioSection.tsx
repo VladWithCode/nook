64ae50cb12b5235ec9304bd7cbf014843b7aff18
vladwb <vladwithcode@gmail.com>
2025-11-21 17:55:26 -0600

    chore: udpate portfolio section cards

--- app/demo/_portfolioSection.tsx
+++ app/demo/_portfolioSection.tsx
@@ -2,6 +2,7 @@ import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import portfolioImg1 from './horsect_1.webp';
 import portfolioImg2 from './horsect_2.webp';
 import Image from 'next/image';
+import { BydLogo } from '@/src/components/svg/byd';
 
 export function PortfolioSection() {
     return (
@@ -10,20 +11,19 @@ export function PortfolioSection() {
                 <ul className="relative flex gap-6">
                     {
                         portfolioItems.map((item) => (
-                            <li className="flex flex-col gap-6 shrink-0 grow-0 w-3/4 py-6" key={item.title}>
+                            <li className="flex flex-col gap-4 shrink-0 grow-0 w-3/4 py-6" key={item.title}>
                                 <div className="w-full aspect-3/4 overflow-hidden">
-                                    {
-                                        item.sourceType === "image"
-                                            ? <Image src={item.src} alt={item.description} className="h-full w-full object-cover scale-105" width={720} height={900} />
-                                            : (
-                                                <video className="h-full w-full object-cover scale-105" muted loop autoPlay playsInline>
-                                                    <source src={item.src} type="video/mp4" />
-                                                </video>
-                                            )
-                                    }
+                                    <ItemMedia item={item} />
                                 </div>
-                                <h3 className="text-xl font-bold uppercase">{item.title}</h3>
-                                <p className="text-sm w-[60%] uppercase font-secondary">{item.description}</p>
+                                <h3 className="text-xl font-secondary uppercase">
+                                    <span className={item.titleIcon ? "sr-only" : undefined}>{item.title}</span>
+                                    {
+                                        item.titleIcon
+                                            ? item.titleIcon
+                                            : null
+                                    }
+                                </h3>
+                                <p className="text-sm text-current/75 w-[60%] uppercase">{item.description}</p>
                             </li>
                         ))
                     }
@@ -36,24 +36,56 @@ export function PortfolioSection() {
 
 const portfolioItems = [
     {
-        title: "Patagonia",
-        description: "An ecommerce experience driven by Patagonia's brand mission.",
+        title: "Byd Durango",
+        // titleIcon: <BydLogo className="h-6" />,
+        titleIcon: null,
+        description: "Una experiencia digital creada para impulsar la nueva era de movilidad eléctrica en México.",
         src: portfolioImg1,
         sourceType: "image",
         mimeType: "image/webp",
-    },
+    } as const,
     {
-        title: "Wilson",
-        description: "A century-old sports brand finding it's place in culture.",
+        title: "Caliente MX",
+        titleIcon: null,
+        description: "Campañas digitales enfocadas en conversión y presencia local en Durango.",
         src: portfolioImg2,
         sourceType: "image",
         mimeType: "image/webp",
-    },
+    } as const,
     {
-        title: "Google Store",
-        description: "An ecommerce experience helping Google bring its hardware to people across the globe.",
+        title: "Q&R",
+        titleIcon: null,
+        description: "Branding y sistema visual profesional para una empresa dedicada a limpieza industrial y residencial.",
         src: "/horsect_3.mp4",
         sourceType: "video",
         mimeType: "video/mp4",
-    },
+    } as const,
+    {
+        title: "Puedes ser tú",
+        titleIcon: null,
+        description: "Identidad visual y estrategia digital para tu negocio!.",
+        src: "",
+        sourceType: null,
+        mimeType: "video/mp4",
+    } as const
 ] as const;
+
+function ItemMedia({ item }: { item: typeof portfolioItems[number] }) {
+    switch (item.sourceType) {
+        case "image":
+            return (
+                <Image src={item.src} alt={item.description} className="h-full w-full object-cover scale-105" width={720} height={900} />
+            );
+        case "video":
+            return (
+                <video className="h-full w-full object-cover scale-105" muted loop autoPlay playsInline>
+                    <source src={item.src} type={item.mimeType} />
+                </video>
+            );
+
+        default:
+            return (
+                <div className="h-full w-full bg-primary"> </div>
+            );
+    }
+}
