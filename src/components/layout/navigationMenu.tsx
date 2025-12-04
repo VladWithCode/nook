571f7e55206a022ca69de5c1f1b6eaa9c852589c
vladwb <vladwithcode@gmail.com>
2025-12-04 13:53:00 -0600

    chore: close navigation menu on navigation

--- src/components/layout/navigationMenu.tsx
+++ src/components/layout/navigationMenu.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { NookLogo } from "../svg/nook";
 import { DuckSVG } from "../svg/duck";
 
@@ -14,45 +14,46 @@ export type TNavigationMenuProps = {
 
 export function NavigationMenu({ }: TNavigationMenuProps) {
     const [isOpen, setIsOpen] = useState(false)
+    const onNavigationCb = useCallback(() => setIsOpen(false), [setIsOpen])
 
     return (
         <>
             <div className="relative z-60 ml-auto pr-2 py-2">
-                <Button size="icon" variant="ghost" onClick={() => setIsOpen((isOpen) => !isOpen)}>
-                    <MenuIcon className="size-6" />
+                <Button className="text-inherit" size="icon" variant="ghost" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+                    <MenuIcon className="size-6 stroke-current" />
                 </Button>
             </div>
             {isOpen ? (
                 <div className="fixed inset-0 z-50 flex flex-col items-center gap-20 bg-main text-gray-50 px-12 py-[25vw]">
-                    <Link href="/">
+                    <Link href="/" onClick={onNavigationCb}>
                         <NookLogo className="h-24" />
                     </Link>
                     <nav>
                         <ul className="text-3xl text-center capitalize space-y-6">
                             {/* <li> */}
-                            {/*     <Link href="/">Inicio</Link> */}
+                            {/*     <Link href="/" onClick={onNavigationCb}>Inicio</Link> */}
                             {/* </li> */}
                             <li>
-                                <Link href="/diseno-y-creatividad">Diseño y Creatividad</Link>
+                                <Link href="/diseno-y-creatividad" onClick={onNavigationCb}>Diseño y Creatividad</Link>
                             </li>
                             <li>
-                                <Link href="/produccion-y-medios">Producción y Medios</Link>
+                                <Link href="/produccion-y-medios" onClick={onNavigationCb}>Producción y Medios</Link>
                             </li>
                             <li>
-                                <Link href="/estrategia-y-experencia">Estrategia y Experiencia</Link>
+                                <Link href="/estrategia-y-experencia" onClick={onNavigationCb}>Estrategia y Experiencia</Link>
                             </li>
                             <li>
-                                <Link href="/descargas-y-activaciones">Descargas y Activaciones</Link>
+                                <Link href="/descargas-y-activaciones" onClick={onNavigationCb}>Descargas y Activaciones</Link>
                             </li>
                         </ul>
                     </nav>
                     <nav>
                         <ul className="flex gap-2 text-sm font-secondary uppercase underline underline-offset-2">
                             <li>
-                                <Link href="/privacidad">Política de Privacidad</Link>
+                                <Link href="/privacidad" onClick={onNavigationCb}>Política de Privacidad</Link>
                             </li>
                             <li>
-                                <Link href="/terminos">Terminos de uso</Link>
+                                <Link href="/terminos" onClick={onNavigationCb}>Terminos de uso</Link>
                             </li>
                         </ul>
                     </nav>
